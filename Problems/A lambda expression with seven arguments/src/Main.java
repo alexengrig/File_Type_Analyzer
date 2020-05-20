f5b1299add01792c999d0cea4ab4415fe54eedf6
Grig Alex <alexengrigdev@gmail.com>
2020-05-20 20:23:38 +0300

    Update course

--- Problems/A lambda expression with seven arguments/src/Main.java
+++ Problems/A lambda expression with seven arguments/src/Main.java
@@ -1,1 +1,2 @@
+// Posted from EduTools plugin
 (a, b, c, d, e, f, g)  -> a.toUpperCase() + b.toUpperCase() + c.toUpperCase() + d.toUpperCase() + e.toUpperCase() + f.toUpperCase() + g.toUpperCase();
